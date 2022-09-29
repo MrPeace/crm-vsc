31dda98f589f1859566a09bb0459c0b2cbb15422
Pete Ostergren <ostergren.pete@gmail.com>
2022-09-29 15:44:30 -0400

    template tags part 8

--- crm/accounts/views.py
+++ crm/accounts/views.py
@@ -1,9 +1,23 @@
+from math import prod
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import *
 
 
 def dashboard(request):
-  return render(request, 'accounts/dashboard.html')
+  orders = Order.objects.all()
+  customers = Customer.objects.all()
+
+  total_orders = orders.count()
+  delivered = orders.filter(status='Delivered').count()
+  pending = orders.filter(status='Pending').count()
+
+  context = {'orders':orders, 'customers':customers,
+  'total_orders':total_orders,
+  'delivered':delivered,
+  'pending':pending}
+  template = 'accounts/dashboard.html'
+  return render(request, template, context)
 
 
 def customer(request):
@@ -11,4 +25,10 @@ def customer(request):
 
 
 def products(request):
-  return render(request, 'accounts/products.html')
+  products = Product.objects.all()
+  context = {
+    'products': products
+  }
+  template = 'accounts/products.html'
+
+  return render(request, template, context)
